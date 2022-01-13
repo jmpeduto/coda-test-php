383863c63daa6be413244383da720269e5229bd7
jmpeduto <jmpeduto@gmail.com>
2022-01-13 11:22:02 -0300

    config: update cloudways config

--- config/autoload/eloquent.global.php
+++ config/autoload/eloquent.global.php
@@ -5,9 +5,9 @@ return [
         'driver'    => 'mysql',
         'host'      => getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost:3306',
         'unix_socket' => getenv('DB_UNIXSOCKET') ? getenv('DB_UNIXSOCKET') : null,
-        'database'  => getenv('DB_DATABASE') ? getenv('DB_DATABASE') : 'codatest',
-        'username'  => getenv('DB_USERNAME') ? getenv('DB_USERNAME') : 'juan',
-        'password'  => getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : '1234',
+        'database'  => getenv('DB_DATABASE') ? getenv('DB_DATABASE') : 'yyemrxauqy',
+        'username'  => getenv('DB_USERNAME') ? getenv('DB_USERNAME') : 'yyemrxauqy',
+        'password'  => getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : 'PRxfbv5a46',
         'charset'   => 'utf8',
         'collation' => 'utf8_general_ci',
     ]
